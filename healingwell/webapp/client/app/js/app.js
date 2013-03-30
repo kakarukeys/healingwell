b5c6e6f79675635fdfb422c9789bf9508564cc11
Wong Jiang Fung <kakarukeys@gmail.com>
2013-03-30 11:04:38 +0800

    added webapp client-side code

---
+++ healingwell/webapp/client/app/js/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Setting up namespace
+var hw = {};
+
+// Declare app level module which depends on filters, and services
+angular.module('healingwell', ['healingwell.filters', 'healingwell.services', 'healingwell.directives'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('', {
+            title: "Healingwell Analytics", 
+            templateUrl: 'partials/home.html', 
+            controller: hw.controller.HomeCtrl
+        })
+        .otherwise({redirectTo: ''});
+    }])
+    .run(['$location', '$rootScope', function($location, $rootScope) {
+        $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+            /* changes title automatically on routing */
+            if (current.$route) {
+                $rootScope.title = current.$route.title;
+            }
+        });
+    }]);
---
+++ healingwell/webapp/client/app/js/controllers.js
@@ -0,0 +1,7 @@
+'use strict';
+
+hw.controller = {};
+
+hw.controller.HomeCtrl = function ($scope) {
+}
+hw.controller.HomeCtrl.$inject = ["$scope"];
