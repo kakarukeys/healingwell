3dda71c2adc64063990a1830bc31691a0d640715
Wong Jiang Fung <kakarukeys@gmail.com>
2013-04-26 22:23:18 +0800

    added models needed for permission handling added principal code for permission handling

--- healingwell/webapp/server/auth/views.py
+++ healingwell/webapp/server/auth/views.py
@@ -1,12 +1,21 @@
-from flask import request, jsonify
+from flask import request, jsonify, session
+import flask.ext.principal as principal
 from healingwell.webapp.server.app import app
-from models import auth
+from models import auth, UserGroup, Group, GroupPermission
+
+principal.Principal(app)
+
+@principal.identity_loaded.connect_via(app)
+def on_identity_loaded(sender, identity):
+    for record in GroupPermission.select(GroupPermission.permission).join(Group).join(UserGroup).where(UserGroup.user == identity.id):
+        identity.provides.add(tuple(record.permission.split(',')))
 
 @app.route("/login", methods=["POST"])
 def login():
     user = auth.authenticate(request.form.get("username", ''), request.form.get("password", ''))
     if user:
         auth.login_user(user)
+        principal.identity_changed.send(app, identity=principal.Identity(user.id))
         outcome = {"message": "Logged in.", "redirect_to": request.args.get("next", '/'), "status": "success"}
     else:
         outcome = {"message": "Invalid username or password.", "status": "error"}
@@ -17,4 +26,11 @@ def login():
 def logout():
     user = auth.get_logged_in_user()
     auth.logout_user(user)
+
+    # Remove session keys set by Flask-Principal
+    for key in ('identity.name', 'identity.auth_type'):
+        session.pop(key, None)
+
+    principal.identity_changed.send(app, identity=principal.AnonymousIdentity())
+
     return jsonify({"message": "Logged out.", "redirect_to": '/', "status": "success"})
